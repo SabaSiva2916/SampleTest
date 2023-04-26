173739fab45e775d8cfbd51edb221613b007ceea
SabaSiva2916 <sabasiva2916@gmail.com>
2023-04-26 12:53:19 +0530

    test

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,9 +1,21 @@
 package org.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java");
+		List<Integer> li = new ArrayList<Integer>();
+		li.add(10);
+		li.add(20);
+		li.add(30);
+		li.add(40);
+		li.add(50);
+
+		System.out.println(li.size());
+		System.out.println(li.get(1));
 
 	}
 }
