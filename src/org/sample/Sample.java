e54a9395964e29e635dcfcde4f92a708c9152a27
SabaSiva2916 <sabasiva2916@gmail.com>
2023-04-26 12:45:47 +0530

    Sample work push

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -4,6 +4,10 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Hello java");
+		String s = "java";
+		System.out.println(s.length());
+		System.out.println(s.charAt(1));
+		System.out.println(s.isEmpty());
 
 	}
 }
